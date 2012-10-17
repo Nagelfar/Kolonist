517ea04618483010b7df3dda9b2bfd2ac7d24e1b
Christian Folie <christian.folie@oebb.at>
2012-10-17 19:49:48 +0200

    added model loader

--- src/app/Kolonist.Web/Models/MapModel.cs
+++ src/app/Kolonist.Web/Models/MapModel.cs
@@ -7,13 +7,18 @@ namespace Kolonist.Web.Models
 {
     public class MapModel
     {
-        public IEnumerable<Tuple<double, double, double>> Vertices { get; set; }
-        public int[] Triangles { get; set; }
+
+        public IEnumerable<double> vertices { get; set; }
+        public int[] faces { get; set; }
+        public int[] materials { get; set; }
+        public int[] uvs { get; set; }
+        public int[] colors { get; set; }
+        public int[] normals { get; set; }
 
 
         private static int IndexForVertex(int x, int y, WorldModel model)
         {
-            return x * model.Width + model.Height;
+            return x * model.Width + y;
         }
 
         internal static MapModel Create(WorldModel world)
@@ -22,15 +27,17 @@ namespace Kolonist.Web.Models
             var vertices = from x in Enumerable.Range(0, world.Width)
                            from y in Enumerable.Range(0, world.Height)
                            let height = random.NextDouble()
-                           select Tuple.Create((double)x, (double)y, height);
+                           select new double[] { (double)x, (double)y, height };
 
             var indices = from x in Enumerable.Range(0, world.Width - 1)
                           from y in Enumerable.Range(0, world.Height - 1)
                           select new[]{
+                              0,
                             IndexForVertex(x,y,world),
                             IndexForVertex(x+1,y,world),
                             IndexForVertex(x+1,y+1,world),
 
+                            0,
                             IndexForVertex(x+1,y+1,world),
                             IndexForVertex(x,y+1,world),
                             IndexForVertex(x,y,world)
@@ -38,8 +45,12 @@ namespace Kolonist.Web.Models
 
             return new MapModel
             {
-                Vertices = vertices.ToArray(),
-                Triangles = indices.SelectMany(x => x).ToArray()
+                vertices = vertices.SelectMany(x => x).ToArray(),
+                faces = indices.SelectMany(x => x).ToArray(),
+                materials = new int[0],
+                colors = new int[0],
+                normals = new int[0],
+                uvs = new int[0],
             };
         }
     }

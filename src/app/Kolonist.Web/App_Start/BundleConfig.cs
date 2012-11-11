7ab32915ac0bdc1093e73eae6b2db5c578dbc744
Christian Folie <christian.folie@gmail.com>
2012-11-11 21:23:35 +0100

    refactored bundles

--- src/app/Kolonist.Web/App_Start/BundleConfig.cs
+++ src/app/Kolonist.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,7 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.Web;
 using System.Web.Optimization;
+using System.Linq;
 
 namespace Kolonist.Web
 {
@@ -8,63 +10,86 @@ namespace Kolonist.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            var allBundles = GetScriptBundles().Concat(GetStyleBundles());
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            foreach(var bundle in allBundles)
+                bundles.Add(bundle);
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+        }
+
+        public static IEnumerable<Bundle> GetScriptBundles()
+        {
+            yield return new ScriptBundle("~/bundles/jquery")
+                .Include(
+                    "~/Scripts/jquery-{version}.js"
+                );
+
+            yield return new ScriptBundle("~/bundles/jqueryui")
+                .Include(
+                    "~/Scripts/jquery-ui-{version}.js"
+                );
+
+            yield return new ScriptBundle("~/bundles/jqueryval")
+                .Include(
+                    "~/Scripts/jquery.unobtrusive*",
+                    "~/Scripts/jquery.validate*"
+                );
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            yield return new ScriptBundle("~/bundles/modernizr")
+                .Include(
+                    "~/Scripts/modernizr-*"
+                );
 
-            bundles.Add(new ScriptBundle("~/bundles/webgl").Include(
+            yield return new ScriptBundle("~/bundles/webgl")
+                .Include(
                     "~/Scripts/glMatrix*",
                     "~/Scripts/three.*"
-                    )
-                 );
+                );
 
-            bundles.Add(new ScriptBundle("~/bundles/kolonist").Include(
+            yield return new ScriptBundle("~/bundles/kolonist")
+                .Include(
                     "~/Scripts/Kolonist.*"
-                    )
                 );
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            yield return new ScriptBundle("~/bundles/bootstrap")
+                .Include(
                     "~/Scripts/bootstrap.*"
-                    )
                 );
-            bundles.Add(new ScriptBundle("~/bundles/less").Include(
+
+            yield return new ScriptBundle("~/bundles/less").Include(
                     "~/Scripts/less.*"
-                    )
+                );
+        }
+
+        public static IEnumerable<Bundle> GetStyleBundles()
+        {
+            yield return new StyleBundle("~/Content/site")
+                .Include("~/Content/site.less");
+
+            yield return new StyleBundle("~/Content/themes/base/css")
+                .Include(
+                    "~/Content/themes/base/jquery.ui.core.css",
+                    "~/Content/themes/base/jquery.ui.resizable.css",
+                    "~/Content/themes/base/jquery.ui.selectable.css",
+                    "~/Content/themes/base/jquery.ui.accordion.css",
+                    "~/Content/themes/base/jquery.ui.autocomplete.css",
+                    "~/Content/themes/base/jquery.ui.button.css",
+                    "~/Content/themes/base/jquery.ui.dialog.css",
+                    "~/Content/themes/base/jquery.ui.slider.css",
+                    "~/Content/themes/base/jquery.ui.tabs.css",
+                    "~/Content/themes/base/jquery.ui.datepicker.css",
+                    "~/Content/themes/base/jquery.ui.progressbar.css",
+                    "~/Content/themes/base/jquery.ui.theme.css"
                 );
 
-            bundles.Add(new StyleBundle("~/Content/site").Include("~/Content/site.less"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css")
-                );
-
-            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+            yield return new StyleBundle("~/Content/bootstrap")
+                .Include(
                     "~/Content/less/bootstrap.less",
                     "~/Content/less/responsive.less"
-                    )
                 );
+
         }
     }
 }

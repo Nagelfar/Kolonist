23001b19e8cac99a691c653837f8d8aa50936a99
Christian Folie <christian.folie@gmail.com>
2012-12-02 17:20:45 +0100

    more tests

--- src/test/Kolonist.Web.Tests/js/Scenarios/WhenUsingUtil.js
+++ src/test/Kolonist.Web.Tests/js/Scenarios/WhenUsingUtil.js
@@ -11,7 +11,7 @@ describe("When calculating nearest power of two", function () {
             expect(result).toBe(number);
         });
     });
-    
+
     describe("And we use 512", function () {
         var number = 512;
         var result = Kolonist.Util.nearestPow2(number);
@@ -37,5 +37,29 @@ describe("When calculating nearest power of two", function () {
         it("should return 1024 as the nearest number to 600", function () {
             expect(result).toBe(1024);
         });
+    });
+});
+
+describe("We calculating rads from degrees", function () {
+    it("Should return 0 on 0 degrees", function () {
+        var degrees = 0;
+
+        var result = Kolonist.Util.Degree2Rad(degrees);
+
+        expect(result).toBe(0);
+    });
+
+    it("should return PI on 180 degrees", function () {
+        var degrees = 180;
+        var result = Kolonist.Util.Degree2Rad(degrees);
+
+        expect(result).toBeCloseTo(Math.PI);
+    });
+
+    it("should return PI/2 on 90 degrees", function () {
+        var degrees = 90;
+        var result = Kolonist.Util.Degree2Rad(degrees);
+
+        expect(result).toBeCloseTo(Math.PI / 2.0);
     });
 });

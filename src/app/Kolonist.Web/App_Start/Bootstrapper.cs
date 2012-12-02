b5a72a2bfda7cd4d95c5d6238dd4eec49960138c
Christian Folie <christian.folie@gmail.com>
2012-12-02 23:22:04 +0100

    basic castle config

---
+++ src/app/Kolonist.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Castle.Windsor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kolonist.Web.App_Start
+{
+    public class Bootstrapper
+    {
+        public static IWindsorContainer BootUp()
+        {
+            var container = new WindsorContainer();
+
+            container.Install(Castle.Windsor.Installer.FromAssembly.This());
+
+            return container;
+        }
+    }
+}
---
+++ src/app/Kolonist.Web/App_Start/MassTransistConfig.cs
@@ -0,0 +1,29 @@
+﻿using Castle.MicroKernel.Registration;
+using MassTransit;
+using MassTransit.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kolonist.Web.App_Start
+{
+    public class MassTransistConfig : IWindsorInstaller
+    {
+        public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
+        {
+            var bus = ServiceBusFactory.New(sbc =>
+            {
+                sbc.UseMsmq();
+                sbc.VerifyMsmqConfiguration();
+                sbc.UseMulticastSubscriptionClient();
+
+                sbc.ReceiveFrom("msmq://localhost/test_queue");
+
+                sbc.Subscribe(subs => subs.LoadFrom(container));
+            });
+
+            container.Register(Component.For<IServiceBus>().Instance(bus));
+        }
+    }
+}

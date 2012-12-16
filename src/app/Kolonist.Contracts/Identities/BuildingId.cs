fbe0666741ba4b49ccd0bfee061ff1195fae77a4
Christian Folie <christian.folie@gmail.com>
2012-12-16 20:25:43 +0100

    identities

---
+++ src/app/Kolonist.Contracts/Identities/BuildingId.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolonist.Contracts.Identities
+{
+    public class BuildingId : AbstractIdentity<Guid>
+    {
+        public BuildingId(Guid id)
+        {
+            Contract.Requires(id != Guid.Empty);
+
+            Id = id;
+        }
+
+        public override Guid Id { get; protected set; }
+    }
+}
---
+++ src/app/Kolonist.Contracts/Identities/BuildingTypeId.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolonist.Contracts.Identities
+{
+    public class BuildingTypeId : AbstractIdentity<Guid>
+    {
+        public BuildingTypeId(Guid id)
+        {
+            Contract.Requires(id != Guid.Empty);
+
+            Id = id;
+        }
+
+        public override Guid Id { get; protected set; }
+    }
+}

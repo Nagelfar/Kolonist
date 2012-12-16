c4eb0e416ddb93b57f447a65ac36df22fd907dc5
Christian Folie <christian.folie@gmail.com>
2012-12-16 21:25:18 +0100

    added validation filter

--- src/app/Kolonist.Web/App_Start/FilterConfig.cs
+++ src/app/Kolonist.Web/App_Start/FilterConfig.cs
@@ -1,19 +1,27 @@
 ﻿using Castle.MicroKernel.Registration;
+using Kolonist.Web.Infrastructure.Mvc;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace Kolonist.Web
 {
-    public class FilterConfig :IWindsorInstaller
+    public class FilterConfig : IWindsorInstaller
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        public static void RegisterGlobalMvcFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
         }
 
+        public static void RegisterGlobalWebApiFilters(System.Web.Http.Filters.HttpFilterCollection filters)
+        {
+            filters.Add(new ValidationActionFilter());
+        }
+
         public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
         {
-            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterGlobalMvcFilters(GlobalFilters.Filters);
+            RegisterGlobalWebApiFilters(GlobalConfiguration.Configuration.Filters);
         }
     }
 }
---
+++ src/app/Kolonist.Web/Infrastructure/Mvc/ValidationActionFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Http.ModelBinding;
+using System.Net;
+
+namespace Kolonist.Web.Infrastructure.Mvc
+{
+    public class ValidationActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+
+                var errors = actionContext.ModelState
+                    .ToDictionary(
+                        x => x.Key,
+                        x => x.Value.Errors.Select(error => error.ErrorMessage)
+                    );
+
+                actionContext.Response =
+                    actionContext.Request.CreateResponse(HttpStatusCode.BadRequest, errors);
+            }
+        }
+    }
+}
